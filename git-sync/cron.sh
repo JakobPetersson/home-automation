1405811292f7ee8728564d7c9479cab266965963
Ubuntu <ubuntu@ubuntu.lan>
2021-02-12 00:54:08 +0100

    compare local branch against remote

--- git-sync/cron.sh
+++ git-sync/cron.sh
@@ -15,15 +15,30 @@ git_fetch() {
   git fetch
 }
 
+git_branches_differ() {
+  local BRANCH_A="${1}"
+  local BRANCH_B="${2}"
+
+  git diff --exit-code "${BRANCH_A}" "${BRANCH_B}" > /dev/null
+}
+
 git_sync() {
   if git_local_changes; then
-    echo "There are local changes, skipping update."
+    echo "There are local changes, skipping sync"
     exit 0
   fi
 
   echo "There are no local changes"
 
   git_fetch
+
+  if git_branches_differ "${LOCAL_BRANCH}" "${REMOTE_BRANCH}"; then
+    echo "Local branch equals remote branch"
+    exit 0
+  fi
+
+  echo "Local branch differs from remote"
+
 }
 
 git_sync
