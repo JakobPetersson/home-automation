bde5298e4c66c8c3aca3c3cdce0f8e393f766e3d
Ubuntu <ubuntu@ubuntu.lan>
2021-02-12 00:44:47 +0100

    fetch changes

--- git-sync/cron.sh
+++ git-sync/cron.sh
@@ -2,15 +2,28 @@
 
 set -eou > /dev/null
 
+readonly LOCAL_BRANCH=main
+readonly REMOTE_BRANCH=origin/main
+
 git_local_changes() {
   local CHANGED_FILES
   CHANGED_FILES=$( git status --porcelain | wc -l )
   [ "${CHANGED_FILES}" -gt 0 ]
 }
 
-if git_local_changes; then
-  echo "There are local changes, skipping update."
-  exit 0
-fi
+git_fetch() {
+  git fetch
+}
 
-echo "There are no local changes"
+git_sync() {
+  if git_local_changes; then
+    echo "There are local changes, skipping update."
+    exit 0
+  fi
+
+  echo "There are no local changes"
+
+  git_fetch
+}
+
+git_sync
