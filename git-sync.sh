3b590b75702e42d010479130e593bb13d725621d
Ubuntu <ubuntu@ubuntu.lan>
2021-02-12 01:31:08 +0100

    Moved git cron script

---
+++ git-sync.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+set -eou > /dev/null
+
+THIS_DIR=$(cd "$(dirname "$0")"; pwd -P)
+
+cd "${THIS_DIR}/.."
+
+readonly LOCAL_BRANCH=main
+readonly REMOTE_BRANCH=origin/main
+
+git_local_changes() {
+  local CHANGED_FILES
+  CHANGED_FILES=$( git status --porcelain | wc -l )
+  [ "${CHANGED_FILES}" -gt 0 ]
+}
+
+git_sync() {
+  if git_local_changes; then
+    echo "There are local changes, skipping sync"
+    exit 0
+  fi
+
+  echo "There are no local changes"
+
+  echo "Fetching"
+  git fetch
+
+  if git diff --exit-code "${LOCAL_BRANCH}" "${REMOTE_BRANCH}" > /dev/null; then
+    echo "Local branch equals remote branch"
+    exit 0
+  fi
+
+  echo "Local branch differs from remote"
+
+  echo "Pulling"
+  git pull
+
+  touch test
+}
+
+git_sync
