269156da599d58684d155df91255e4556b71c592
Jakob Petersson <JakobPetersson@users.noreply.github.com>
2021-06-20 00:45:59 +0200

    Update appdaemon to 4.0.8

--- containers/appdaemon/setup.sh
+++ containers/appdaemon/setup.sh
@@ -6,7 +6,7 @@ THIS_DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 APPDAEMON_GIT_DIR="${THIS_DIR}/appdaemon-git"
 
-APPDAEMON_VERSION=4.0.5
+APPDAEMON_VERSION=4.0.8
 
 if [ ! -d "${APPDAEMON_GIT_DIR}" ]; then
   git clone https://github.com/AppDaemon/appdaemon.git "${APPDAEMON_GIT_DIR}"
