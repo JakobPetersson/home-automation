2584481f20b0d3c0ec82116b127cbe2be8f83689
Jakob Petersson <JakobPetersson@users.noreply.github.com>
2021-07-01 00:35:42 +0200

    Take time_fired

--- containers/appdaemon/config/apps/area/area.py
+++ containers/appdaemon/config/apps/area/area.py
@@ -15,7 +15,7 @@ class Area(hass.Hass):
         await self.init_sub_areas()
 
         self.state = {}
-        await self._update({
+        await self._update(None, {
             "on": False,
             "kelvin": 3500,
             "brightness_pct": 100
@@ -35,57 +35,68 @@ class Area(hass.Hass):
     # Services (Called from  other apps)
     #
 
-
-    async def service_turn_on_manual(self):
-        await self._update({
-            "on": True
-        })
-
-
-    async def service_turn_off_manual(self):
-        await self._update({
-            "on": False
-        })
-
-
-    async def service_dim_up_manual(self):
-        await self._update({
-            "on": True,
-            "brightness_pct": min(100, self.state["brightness_pct"] + 5)
-        })
-
-
-    async def service_dim_down_manual(self):
-        if self.state["on"]:
-            await self._update({
-                "brightness_pct": max(1, self.state["brightness_pct"] - 5)
+    async def service_manual(self, time_fired, cmd_name):
+        if cmd_name == "on":
+            await self._update(time_fired, {
+                "on": True
             })
+        elif cmd_name == "off":
+            await self._update(time_fired, {
+                "on": False
+            })
+        elif cmd_name == "dim_up":
+            await self._update(time_fired, {
+                "on": True,
+                "brightness_pct": min(100, self.state["brightness_pct"] + 5)
+            })
+        elif cmd_name == "dim_down":
+            if self.state["on"]:
+                await self._update(time_fired, {
+                    "brightness_pct": max(1, self.state["brightness_pct"] - 5)
+                })
+
+
+    async def service_turn_on_manual(self, time_fired):
+        await self.service_manual(time_fired, "on")
+
+
+    async def service_turn_off_manual(self, time_fired):
+        await self.service_manual(time_fired, "off")
+
+
+    async def service_dim_up_manual(self, time_fired):
+        await self.service_manual(time_fired, "dim_up")
+
+
+    async def service_dim_down_manual(self, time_fired):
+        await self.service_manual(time_fired, "dim_down")
 
     #
     #
     #
 
 
-    async def _update(self, state_update):
+    async def _update(self, time_fired, state_update):
+        self.log(time_fired)
         new_state = {**self.state, **state_update}
 
         if new_state != self.state:
             self.state = new_state
             self.log("Updated: {}".format(self.state))
-            await self._update_area()
+            await self._update_area(time_fired)
 
-        await self._update_sub_areas(state_update)
+        await self._update_sub_areas(time_fired, state_update)
 
 
-    async def _update_sub_areas(self, state_update):
+    async def _update_sub_areas(self, time_fired, state_update):
         for sub_area in self.sub_areas:
-            await self.create_task(sub_area._update(state_update))
+            await self.create_task(sub_area._update(time_fired, state_update))
 
 
     #
     # Update
     #
-    async def _update_area(self):
+    async def _update_area(self, time_fired):
         if not self.area_id:
             return
 
