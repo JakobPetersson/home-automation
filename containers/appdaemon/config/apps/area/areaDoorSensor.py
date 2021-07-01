5e477e504fec23aa22d367c89d82ddd178460f8c
Jakob Petersson <JakobPetersson@users.noreply.github.com>
2021-07-02 01:40:44 +0200

    AreaDoorSensor extends AreaDevice

--- containers/appdaemon/config/apps/area/areaDoorSensor.py
+++ containers/appdaemon/config/apps/area/areaDoorSensor.py
@@ -1,30 +1,12 @@
-import hassapi as hass
+import areaDevice
 import datetime
 
 
-class AreaDoorSensor(hass.Hass):
+class AreaDoorSensor(areaDevice.AreaDevice):
 
     async def initialize(self):
-        await self.init_area()
-        await self.init_device()
-
-    async def init_area(self):
-        self.area = None
-        area_name = self.args.get("area_name")
-        if area_name:
-            self.area = await self.get_app(area_name)
-            if not self.area:
-                self.log("Area not initialized, {}".format(area_name))
-        else:
-            pass
-
-    async def init_device(self):
-        device_ieee = self.args.get("device_ieee")
-        self.event_listen_handle = await self.listen_event(
-            self.event_cb,
-            "zha_event",
-            device_ieee=device_ieee
-        )
+        await super().initialize()
+        self.log("AreaDoorSensor")
 
     async def event_cb(self, event_name, data, kwargs):
         command = data["command"]
@@ -38,4 +20,4 @@ class AreaDoorSensor(hass.Hass):
                 await self.area.service_automated(time_fired, "off")
 
     async def terminate(self):
-        await self.cancel_listen_event(self.event_listen_handle)
+        await super().terminate()
