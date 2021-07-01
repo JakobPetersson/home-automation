69297f0548914de5acee4028b5b1ec722293dff6
Jakob Petersson <JakobPetersson@users.noreply.github.com>
2021-07-02 01:54:19 +0200

    Move all AreaDevice classes to same module

--- containers/appdaemon/config/apps/area/areaDevice.py
+++ containers/appdaemon/config/apps/area/areaDevice.py
@@ -1,4 +1,5 @@
 import hassapi as hass
+import datetime
 
 
 class AreaDevice(hass.Hass):
@@ -27,3 +28,76 @@ class AreaDevice(hass.Hass):
 
     async def terminate(self):
         await self.cancel_listen_event(self.event_listen_handle)
+
+
+class AreaButton(AreaDevice):
+
+    async def initialize(self):
+        await super().initialize()
+        await self.init_dimmer()
+        self.dim_interval_s = 0.2
+
+    async def init_dimmer(self):
+        self.dimmer_timer_handle = None
+
+    async def event_cb(self, event_name, data, kwargs):
+        command = data["command"]
+        time_fired = datetime.datetime.fromisoformat(data["metadata"]["time_fired"])
+
+        await self.cancel_dimmer_timer()
+        if command == "on":
+            await self.area.service_manual(time_fired, "on")
+        elif command == "off":
+            await self.area.service_manual(time_fired, "off")
+        elif command == "move_with_on_off":
+            self.dimmer_timer_handle = await self.run_every(
+                self.dim_up,
+                "now",
+                self.dim_interval_s
+            )
+        elif command == "move":
+            self.dimmer_timer_handle = await self.run_every(
+                self.dim_down,
+                "now",
+                self.dim_interval_s
+            )
+        elif command == "stop":
+            pass
+
+    async def cancel_dimmer_timer(self):
+        if self.dimmer_timer_handle != None:
+            await self.cancel_timer(self.dimmer_timer_handle)
+            self.dimmer_timer_handle = None
+
+    async def dim_up(self, kwargs):
+        if self.area:
+            time_fired = datetime.datetime.now(datetime.timezone.utc)
+            await self.area.service_manual(time_fired, "dim_up")
+
+    async def dim_down(self, kwargs):
+        if self.area:
+            time_fired = datetime.datetime.now(datetime.timezone.utc)
+            await self.area.service_manual(time_fired, "dim_down")
+
+    async def terminate(self):
+        await super().terminate()
+
+
+class AreaDoorSensor(AreaDevice):
+
+    async def initialize(self):
+        await super().initialize()
+
+    async def event_cb(self, event_name, data, kwargs):
+        command = data["command"]
+        args = data["args"]
+        time_fired = datetime.datetime.fromisoformat(data["metadata"]["time_fired"])
+
+        if command == "attribute_updated" and args["attribute_name"] == "on_off":
+            if args["value"] == 1:
+                await self.area.service_automated(time_fired, "on")
+            if args["value"] == 0:
+                await self.area.service_automated(time_fired, "off")
+
+    async def terminate(self):
+        await super().terminate()
