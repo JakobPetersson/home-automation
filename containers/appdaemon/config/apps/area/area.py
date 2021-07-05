97a1b4b9978457eac2017bbd30f9cf9e79f83bb8
Ubuntu <ubuntu@ubuntu.lan>
2021-07-05 22:50:19 +0200

    Change order of methods

--- containers/appdaemon/config/apps/area/area.py
+++ containers/appdaemon/config/apps/area/area.py
@@ -46,30 +46,6 @@ class Area(hass.Hass):
     #
     #
 
-    async def update_light_state(self, light_state_update, time_fired):
-        # self.log(time_fired)
-
-        # Get new state by applying state update to current state
-        new_state = {**self.light_state, **light_state_update}
-
-        # Perform actions on this area if state is changed
-        if time_fired < self.last_update:
-            self.log("Old update!")
-        elif new_state != self.light_state:
-            # Update state to new state
-            self.last_update = time_fired
-            self.light_state = new_state
-            # self.log("Updated: {}".format(self.light_state))
-            await self._update_area()
-
-        # Propagate light state to all sub areas
-        for sub_area in self.sub_areas:
-            await self.create_task(sub_area.update_light_state(self.light_state, time_fired))
-
-    #
-    #
-    #
-
     async def _service(self, cmd_name, time_fired):
         if cmd_name == "on":
             await self.update_light_state({
@@ -89,6 +65,30 @@ class Area(hass.Hass):
                 await self.update_light_state({
                     "brightness_pct": max(1, self.light_state["brightness_pct"] - 5)
                 }, time_fired)
+
+    #
+    #
+    #
+
+    async def update_light_state(self, light_state_update, time_fired):
+        # self.log(time_fired)
+
+        # Get new state by applying state update to current state
+        new_state = {**self.light_state, **light_state_update}
+
+        # Perform actions on this area if state is changed
+        if time_fired < self.last_update:
+            self.log("Old update!")
+        elif new_state != self.light_state:
+            # Update state to new state
+            self.last_update = time_fired
+            self.light_state = new_state
+            # self.log("Updated: {}".format(self.light_state))
+            await self._update_area()
+
+        # Propagate light state to all sub areas
+        for sub_area in self.sub_areas:
+            await self.create_task(sub_area.update_light_state(self.light_state, time_fired))
 
     async def _update_area(self):
         if not self.area_id:
